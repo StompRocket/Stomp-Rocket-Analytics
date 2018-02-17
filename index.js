2c082d7517af53dbb11e4d12d5b1fb3086ba9f61
Ronan <rsf.sho@gmail.com>
2018-02-17 07:33:49 -0800

    Add Firebase

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var express = require('express')
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var admin = require("firebase-admin");
 
+var serviceAccount = require("sr-analytics-a968c-firebase-adminsdk-q3ulc-a1901f7d4d.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sr-analytics-a968c.firebaseio.com"
+});
 app.use(express.static('client'))
 
 io.on('connection', function(socket) {
